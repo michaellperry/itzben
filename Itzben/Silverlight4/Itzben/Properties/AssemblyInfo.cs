226ca1137e5bf9c73be76919153aad7e4dd79da7
Michael L Perry <michael@qedcode.com>
2011-01-24 15:40:08 +0000

    Initial check in.

---
+++ Itzben/Silverlight4/Itzben/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Itzben")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ABSG")]
+[assembly: AssemblyProduct("Itzben")]
+[assembly: AssemblyCopyright("Copyright © ABSG 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("41fa1bff-e4c5-466f-90ae-3c5e030f4c55")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Itzben/WPF/Itzben/EnumEqualsConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Itzben
+{
+	// Many thanks to Lars and Scott, whose answers I combined.
+	// http://stackoverflow.com/questions/397556/wpf-how-to-bind-radiobuttons-to-an-enum
+	public class EnumEqualsConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType != typeof(bool) && targetType != typeof(bool?))
+				throw new InvalidOperationException("EnumEqualsConverter can only be used with boolean properties.");
+
+			return value.ToString() == parameter.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value.Equals(false))
+				return Binding.DoNothing;
+			else
+				return Enum.Parse(targetType, parameter.ToString());
+		}
+	}
+}
