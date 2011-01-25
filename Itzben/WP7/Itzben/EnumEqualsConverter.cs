4b6ff59518d3bc92031cae0bca8c35fe30620d90
Michael L Perry <michael@qedcode.com>
2011-01-25 04:29:42 +0000

    Added Windows Phone 7 library.

---
+++ Itzben/WP7/Itzben/EnumEqualsConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Itzben
+{
+	// Many thanks to Lars and Scott, whose answers I combined.
+	// http://stackoverflow.com/questions/397556/wpf-how-to-bind-radiobuttons-to-an-enum
+	public class EnumEqualsConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType != typeof(bool) && targetType != typeof(bool?))
+				throw new InvalidOperationException("EnumEqualsConverter can only be used with boolean properties.");
+
+			return value.ToString() == parameter.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value.Equals(false))
+				return DependencyProperty.UnsetValue;
+			else
+				return Enum.Parse(targetType, parameter.ToString(), false);
+		}
+	}
+}
