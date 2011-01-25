d8d510be0e40f4b333b6d7eb569fe907093c0f8a
Michael L Perry <michael@qedcode.com>
2011-01-25 22:09:23 +0000

    Added VisibleWhenEqualConverter.

---
+++ Itzben/WPF/Itzben/VisibleWhenEqualConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Itzben
+{
+	public class VisibleWhenEqualConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType != typeof(Visibility) && targetType != typeof(Visibility?))
+				throw new InvalidOperationException("VisibleWhenEqualConverter can only be used with Visibility properties.");
+
+			return value.ToString() == parameter.ToString() ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
